6106acc571efb4c3577fe80379919d5701122959
Bryan Stern <stern.bry@gmail.com>
2014-07-20 16:26:33 -0700

    layout adjustment

--- app/src/main/java/com/bryanstern/playground/MainActivity.java
+++ app/src/main/java/com/bryanstern/playground/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bryanstern.playground.api.GitHubService;
